042245ebfe7a6c5342cde9b459e4edaad0af9297
Sankalp Bhatt <glitter.sankalp@gmail.com>
2017-01-11 16:01:52 +0530

    Momdify Autoload

--- autoload.php
+++ autoload.php
@@ -2,10 +2,12 @@
 
 spl_autoload_register(function ($classname) {
     $dir = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
-    if (strstr($classname, "\\QueueSystem") === 0) {
+    if (strpos($classname, "QueueSystem\\SQS\\") === 0) {
+        $file = $dir . "SQS/".basename(str_replace('\\', '/', $classname)) . '.php';
+        if (file_exists($file)) require $file;
+    }else if (strpos($classname, "QueueSystem\\") === 0) {
         $file = $dir . basename(str_replace('\\', '/', $classname)) . '.php';
-        echo $file;
-        echo PHP_EOL;
         if (file_exists($file)) require $file;
     }
+    
 });
